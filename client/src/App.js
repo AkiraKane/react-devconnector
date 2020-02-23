54d64ca87e23460729638934b5b01d36e93f8d7d
AkiraKane <vampire1987116hm@gmail.com>
2020-02-23 14:13:56 -0800

    add edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import { Landing } from "./components/layout/Landing";
+import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-form/CreateProfile";
+import EditProfile from "./components/profile-form/EditProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
@@ -36,6 +38,16 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
             </Switch>
           </section>
         </Fragment>
